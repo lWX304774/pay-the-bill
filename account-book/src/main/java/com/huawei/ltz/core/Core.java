68b9b65f197b8a6d8741ec279ab6c3231d0ff9e8
lWX304774 <lWX304774@gmail.com>
2018-02-19 13:09:00 +0800

    init this proj
    add pom && main && test

---
+++ account-book/src/main/java/com/huawei/ltz/core/Core.java
@@ -0,0 +1,14 @@
+package com.huawei.ltz.core;
+
+/**
+ * Hello world!
+ *
+ */
+public class Core
+{
+    
+    public static void main(String[] args)
+    {
+        System.out.println("Hello World!");
+    }
+}
