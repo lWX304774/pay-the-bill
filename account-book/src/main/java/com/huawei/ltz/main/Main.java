8a50fbdf099184262bc05286b3b1ddbb1a64f23d
lWX304774 <lWX304774@gmail.com>
2018-02-20 23:33:15 +0800

    2018-2-20

--- account-book/src/main/java/com/huawei/ltz/main/Main.java
+++ account-book/src/main/java/com/huawei/ltz/main/Main.java
@@ -1,14 +1,17 @@
-package com.huawei.ltz.core;
+package com.huawei.ltz.main;
+
+import com.huawei.ltz.util.ReadExcel;
 
 /**
  * Hello world!
  *
  */
-public class Core
+public class Main
 {
     
     public static void main(String[] args)
     {
         System.out.println("Hello World!");
+        ReadExcel.readExcelFrom("");
     }
 }
